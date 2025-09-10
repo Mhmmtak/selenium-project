f4b7484dbdcad0b73f95bb94afea5d248b32cfd7
muhammetakdeniz <akmuhammett3483@gmail.com>
2025-09-09 22:18:13 -0500

    practice

--- src/test/java/com/cydeo/tests/day08_properties_config_reader/T4_use_config_reader.java
+++ src/test/java/com/cydeo/tests/day08_properties_config_reader/T4_use_config_reader.java
@@ -1,4 +1,18 @@
 package com.cydeo.tests.day08_properties_config_reader;
 
+import org.testng.annotations.Test;
+
 public class T4_use_config_reader {
+//TC #4: Google search
+
+    @Test
+    public void google_search_test(){
+
+        //1- Open a chrome browser
+//2- Go to: https://google.com
+//3- Write “apple” in search box
+//4- Verify title:
+//Expected: apple - Google Search
+
+    }
 }
