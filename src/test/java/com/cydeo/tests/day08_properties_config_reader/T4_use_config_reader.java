e05ea6e96df84ad6f455d7fad8c73cce6757b3ce
muhammetakdeniz <akmuhammett3483@gmail.com>
2025-09-09 22:20:53 -0500

    practice..

--- src/test/java/com/cydeo/tests/day08_properties_config_reader/T4_use_config_reader.java
+++ src/test/java/com/cydeo/tests/day08_properties_config_reader/T4_use_config_reader.java
@@ -8,7 +8,7 @@ public class T4_use_config_reader {
     @Test
     public void google_search_test(){
 
-        //1- Open a chrome browser
+//1- Open a chrome browser
 //2- Go to: https://google.com
 //3- Write “apple” in search box
 //4- Verify title:
